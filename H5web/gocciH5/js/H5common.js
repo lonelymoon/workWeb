28b11694cc64cd907c26a734788cfb5f2de77019
zao <z454236029@gmail.com>
2017-05-09 20:21:50 +0800

    H5Web

---
+++ H5web/gocciH5/js/H5common.js
@@ -0,0 +1,13 @@
+(function(){
+	var windowWidth = window.innerWidth,
+	trueWidth = windowWidth >= 750? 750 : windowWidth;
+
+	if(document.querySelector('html'))
+	document.querySelector('html').style.fontSize = trueWidth / 6.25 + "px";
+	else {
+		var style = document.createElement('style');
+		style.type = "text/css";
+		style.innerHTML = "html{font-size:"+trueWidth / 6.25 +"px;}";
+		document.querySelector('head').appendChild(style);
+	}
+})();
