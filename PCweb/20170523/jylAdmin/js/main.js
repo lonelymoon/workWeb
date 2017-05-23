e5198e08c096c6bcb682bd618e6567e94501906d
zao <z454236029@gmail.com>
2017-05-23 18:44:15 +0800

    PCWeb

---
+++ PCweb/20170523/jylAdmin/js/main.js
@@ -0,0 +1,93 @@
+jQuery(function($){
+
+var startIdx = 0,
+	countNum = 15,
+	hasCreateList = false,
+	scroll = new IScroll('.tableBox',{
+		mouseWheel : true
+	});
+
+function getData(tel){
+	var strflg = 0;
+	tel = tel || "";
+	if(tel) strflg = 1;
+	$.ajax({  
+		type:'get',
+	    url:'ladmin/Getusermessage.action',
+	    data:{intstart:startIdx,intcount:countNum,strphone:tel,strflg:strflg},   
+	    dataType:'json',
+	    success:function(data)
+    	{             	
+    		var totalNum = data.intcount,
+    			pageNum = Math.ceil(totalNum / countNum),
+    			array = data.jsonarray;
+
+    		createPageList(pageNum);
+    		resetHtml(array);
+    	}   	    
+	});   
+}
+
+function createPageList(pageNum){
+	if(hasCreateList){
+		return false;
+	}
+
+	hasCreateList = true;
+
+	$(".pageListBox").Page({
+	    totalPages: pageNum,
+	    liNums: 5,
+	    activeClass: 'activP',
+	    callBack : function(page){
+	      	startIdx = ( page - 1 ) * countNum;
+	      	getData();
+	    }
+	});
+};
+
+$('.search-btn').on('click',function(e){
+	var tel = $('#search').val();
+
+	hasCreateList = false;
+	getData(tel);
+
+});
+
+document.onkeyup = function(e){
+	e = e || event;
+	if(e.keyCode == 13){
+		$('.search-btn').trigger('click');
+	}
+};
+
+function resetHtml(dataArray){
+	var temp = "";
+	for( var i = 0 , obj; obj = dataArray[i++]; ){
+		var tpl = '<div class="row bodyRow">'+
+					'<div class="time item">'+obj.strpapercreatetime+'</div>'+
+					'<div class="name item">'+obj.strpapername+'</div>'+
+					'<div class="tel item">'+obj.strpaperphone+'</div>'+
+					'<div class="local item">'+obj.strpaperaddress+'</div>'+
+					'<div class="area item">'+obj.strpaperarea+'</div>'+
+					'<div class="type item">'+obj.strpapersetup+'</div>'+
+					'<div class="fixTime item">'+obj.strpaperdecoration+'</div>'+
+					'<div class="furn item">'+obj.strpaperfurniture+'</div>'+
+					'<div class="baby item">'+obj.strpapergravida+'</div>'+
+					'<div class="plan item">'+obj.strpaperjoin+'</div>'+
+					'<div class="checkTime item">'+obj.strpapercome+'</div>'+
+					'<div class="describe item">'+obj.strpapermessage+'</div>'+
+					'<div class="clear"></div>'+
+				'</div>';
+		temp += tpl;
+	}
+
+	$('.tableBody').html(temp);
+	setTimeout(function(){
+		scroll.refresh();
+	},100);
+};
+
+getData();
+
+});
