1a4005898eaf256bc364914be48ef8afa70ccf23
zao <z454236029@gmail.com>
2018-03-26 19:09:22 +0800

    H5Web

--- H5web/tencent20180316/js/share.js
+++ H5web/tencent20180316/js/share.js
@@ -4,7 +4,7 @@ var signurl = window.location.href;
 var TITLE = "腾讯唤新计划宣讲会邀请", //微信标题
     DESC = "诚邀您开启2018唤新旅程，挖掘互联网新商机！",  //微信描叙
     IMGURL = "https://web.aochey.com/tencentHuanxin/share.jpg?ver="+new Date().getTime(), //分享图片地址
-    LINK = "https://web.aochey.com/tencentHuanxin/"; //分享来源链接
+    LINK = signurl; //分享来源链接
 
 $.ajax({
     type:'post',
@@ -51,7 +51,7 @@ function goReady(){
         // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
         wx.onMenuShareTimeline({
             title: TITLE,
-            link: LINK,
+            link: encodeURIComponent(LINK),
             imgUrl: IMGURL,
             trigger: function (res) {         
             },
